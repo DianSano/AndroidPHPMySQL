9f5bb06254a15e14a413b8e0dd14803ab5bb756d
DianSano <alb.vds@gmail.com>
2019-09-04 22:35:54 +0700

    versi 4

--- app/src/main/java/id/co/blogspot/diansano/androidphpmysql/Constants.java
+++ app/src/main/java/id/co/blogspot/diansano/androidphpmysql/Constants.java
@@ -3,7 +3,7 @@ package id.co.blogspot.diansano.androidphpmysql;
 public class Constants {
 
     //dont use localhost but ip address (localhost may not work)
-    public static final String ROOT_URL = "http://192.168.1.20/android/v1/";
+    public static final String ROOT_URL = "http://192.168.1.101/android/v1/";
 
     public static final String URL_REGISTER = ROOT_URL + "registerUser.php";
     public static final String URL_LOGIN = ROOT_URL + "userLogin.php";
